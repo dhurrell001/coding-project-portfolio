d2be383c7d66560c53cdbf728c03071ec49de815
David Hurrell <dhurrell@hotmail.co.uk>
2025-06-13 12:20:20 +0100

    Refactor Navigation component to use NavLink for routing and update CSS for improved layoutand responsive layout

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,21 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Navigation.module.css";
 
-export default function Navigation() {
-  function handleHomeClick() {
-    alert("Home button clicked");
-  }
+// This component defines a navigation bar with links to Home, Projects, and About Me pages.
+// It uses NavLink from react-router-dom to create links.
 
+export default function Navigation() {
   return (
     <nav className={styles["navigation-container"]}>
-      <button className={styles["nav-button"]} onClick={handleHomeClick}>
+      <NavLink to="/" className={styles["nav-button"]}>
         Home
-      </button>
-      <button className={styles["nav-button"]}>Projects</button>
-      <button className={styles["nav-button"]}>About Me</button>
+      </NavLink>
+      <NavLink to="/cv" className={styles["nav-button"]}>
+        Curriculum Vitae
+      </NavLink>
+      <NavLink to="/about" className={styles["nav-button"]}>
+        About Me
+      </NavLink>
     </nav>
   );
 }
