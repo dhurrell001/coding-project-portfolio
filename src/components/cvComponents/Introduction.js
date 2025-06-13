a09eaee89d0b5fafcdf22e1ffcdb04293c6914de
David Hurrell <dhurrell@hotmail.co.uk>
2025-06-13 12:23:43 +0100

    Add Introduction component to showcase web development aspirations and skills

---
+++ src/components/cvComponents/Introduction.js
@@ -0,0 +1,18 @@
+export default function Introduction() {
+  return (
+    <div className="introduction">
+      <p>
+        I am an aspiring web developer with a strong foundation in both
+        technical and transferable skills, gained through my extensive
+        experience in the plastering industry.
+      </p>
+      <p>
+        With over 20 years of running a successful plastering business I have
+        honed my problem-solving, leadership, and client management abilities,
+        all of which I am eager to apply to the tech field. Recently, I have
+        completed relevant entry-level qualifications in web development and
+        programming, including JavaScript and Python.
+      </p>
+    </div>
+  );
+}
