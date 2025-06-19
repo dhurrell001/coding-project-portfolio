4b1eb0218a0864c61f89e487339371887551071f
David Hurrell <dhurrell@hotmail.co.uk>
2025-06-19 18:03:36 +0100

    Refactor comments in TitleBar component for clarity

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,5 +1,7 @@
 import styles from "./TitleBar.module.css";
 import Navigation from "./Navigation";
+// This component defines the title bar of the application.
+// It includes the main title and the navigation bar.
 export default function TitleBar() {
   return (
     <div className={styles["title-bar"]}>
