45260c7eefa5070ce4af05c5c0c5304546d939e3
David Hurrell <dhurrell@hotmail.co.uk>
2025-06-19 18:03:24 +0100

    Fix comment to clarify the source of project tiles in ProjectTileLayout component

--- src/components/ProjectTileLayout.js
+++ src/components/ProjectTileLayout.js
@@ -1,7 +1,7 @@
 import styles from "./ProjectTileLayout.module.css";
 
 // This component serves as a layout for project tiles
-// The childre will be the individual project tiles
+// The childre will be the individual project tiles passed in from the HomePage component.
 
 export default function ProjectTileLayout({ children }) {
   return <div className={styles["project-container"]}>{children}</div>;
