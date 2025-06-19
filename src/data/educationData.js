106a4bc60f69e1eed140e46614d6085280e129b4
David Hurrell <dhurrell@hotmail.co.uk>
2025-06-19 18:05:14 +0100

    Add educationEntries data for educational qualifications

---
+++ src/data/educationData.js
@@ -0,0 +1,28 @@
+export const educationEntries = [
+  {
+    institution: "Open University",
+    qualification: "Access to Higher Education STEM ",
+    year: "2022 - 2023",
+    grade: "Distinction",
+  },
+  {
+    institution: "Weston College/Bristol Training Institute",
+    qualification: "Python Programming bootcamp",
+    year: "2023",
+
+    grade: "Pass",
+  },
+  {
+    institution: "Weston College",
+    qualification: "BTEC National Diploma in Computing",
+    year: "1991 - 1993",
+    grade: "Distinction",
+  },
+  {
+    institution: "Weston College",
+    qualification: "BTEC First Diploma in Business and Finance",
+    year: "1990 - 1991",
+
+    grade: "Merit",
+  },
+];
